0aa8eb47bface41ff838658caed3fb94d8d0f6e5
Bulku <leoteobulku@gmail.com>
2023-10-10 07:13:38 -0500

    Identacion y correciones a la ortografia

--- components/EducationItem.tsx
+++ components/EducationItem.tsx
@@ -9,12 +9,12 @@ interface EducationItemProps {
     }
 function EducationItem({ almaMatter, fechas, grado, descripcion, estado}: EducationItemProps) {
   return (
-    <div className="bg-neutral-50 pt-5 px-10 w-90 min-w-max rounded-xl m-2 hover:scale-95 ">
+    <div className="bg-[#fbbf24] pt-5 px-10 w-90 rounded-xl m-2 hover:scale-95 max-w-[80%] mx-auto  ">
         <div className="grid grid-cols-2 grid-flow-row gap-4">
             <h1 style={{fontWeight: 'bold', fontSize: 20 }}>{almaMatter}</h1>
             <div className="flex flex-between place-items-center gap-4 " >
-              <h1 style={{ color: 'black', backgroundColor: '#ffffff', fontWeight: 'bold', fontSize: '16px' }}>Estudiante</h1>
-              <h1 className="px-4 text-" style={{ color: 'black', backgroundColor: '#d4b353', fontWeight: 'bold', fontSize: '16px' }}>{fechas}</h1>
+              <h1 className='rounded-xl' style={{ color: 'black', backgroundColor: '#ffffff', fontWeight: 'bold', fontSize: '16px' }}>Estudiante</h1>
+              <h1 className="px-4 text- rounded-xl " style={{ color: 'white', backgroundColor: '#000000', fontWeight: 'bold', fontSize: '16px' }}>{fechas}</h1>
 
             </div>
         </div>
@@ -22,7 +22,7 @@ function EducationItem({ almaMatter, fechas, grado, descripcion, estado}: Educat
             <h1 style={{fontWeight: 500, fontSize: 20}}>{grado}</h1>
             <h1 style={{ color: "black",  }}>{descripcion}</h1>
 
-            <h1 style={{color: "#d4b353" , fontWeight: 'bold', fontSize: '15px'}}>{estado}</h1>
+            <h1 className='rounded-xl 'style={{color: "#000000" , fontWeight: 'bold', fontSize: '15px'}}>{estado}</h1>
         </div>
     </div>
   )
